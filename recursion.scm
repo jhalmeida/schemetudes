d30772a6d769986a3650265e53717074862d7e7f
justin almeida <justin.h.almeida@gmail.com>
2017-09-04 22:45:47 -0700

    sum list function

--- recursion.scm
+++ recursion.scm
@@ -9,3 +9,17 @@
     (if (= n 1)
       c
       (facttail (- n 1) (* c n)))))
+
+;A function that counts the number of list items, my-length.
+(define (my-length l c)
+  (if (equal? (cdr l) '())
+    (+ c 1)
+    (my-length (cdr l) (+ c 1))))
+
+; A function that summarizes numbers in a list
+(define (sum-list l c)
+  (if (equal? (cdr l) '())
+    (+ (car l) c)
+    (sum-list (cdr l) (+ (car l) c))))
+
+;A function that takes a list (ls) and an object (x) as arguments and returns a list removing x from ls
