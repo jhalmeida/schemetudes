157bedcc6049f3cc982436951cd52189f536c297
justin almeida <justin.h.almeida@gmail.com>
2017-09-06 20:50:35 -0700

    partial square sum root

--- hof.scm
+++ hof.scm
@@ -18,3 +18,7 @@
 ;filtering out numbers (x) that are not 10 ≤ x ≤ 100.
 (define (filter-10-100 l1)
   (keep-matching-items l1 (lambda (x) (<= 10 x 100))))
+
+;Write a function that squares each item of a list, then sums them and then makes square root of it.
+(define (squares-sum-root l)
+  (let* ((squares (map (lambda (x) (* x x)))) (sum (for-each )))))
